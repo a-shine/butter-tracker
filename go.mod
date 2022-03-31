7565d6d15cb41a7754cd771bae4f67d921503d0d
Alexandre Shinebourne <alexandre@shinebourne.com>
2022-03-31 16:51:57 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/butter-tracker
+
+go 1.17
+
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/lib/pq v1.10.4
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+var todoList []string
+
+func getCmd(input string) string {
+	inputArr := strings.Split(input, " ")
+	return inputArr[0]
+}
+
+func getMessage(input string) string {
+	inputArr := strings.Split(input, " ")
+	var result string
+	for i := 1; i < len(inputArr); i++ {
+		result += inputArr[i]
+	}
+	return result
+}
+
+func updateTodoList(input string) {
+	tmpList := todoList
+	todoList = []string{}
+	for _, val := range tmpList {
+		if val == input {
+			continue
+		}
+		todoList = append(todoList, val)
+	}
+}
+
+func main() {
+	db := ConnectToDB()
+	query := "SELECT * FROM nodes"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(rows)
+	http.HandleFunc("/network", func(w http.ResponseWriter, r *http.Request) {
+		// Upgrade upgrades the HTTP server connection to the WebSocket protocol.
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade failed: ", err)
+			return
+		}
+		defer conn.Close()
+
+		// Continuously read and write message
+		for {
+			output := "Current Todos: \n"
+			for _, todo := range todoList {
+				output += "\n - " + todo + "\n"
+			}
+			output += "\n----------------------------------------"
+			message := []byte(output)
+			err = conn.WriteMessage(1, message)
+			if err != nil {
+				log.Println("write failed:", err)
+				break
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.ListenAndServe(":8080", nil)
+}
